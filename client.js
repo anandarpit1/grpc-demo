47f6912f629f93a440dcebee6fc23745d91d5403
Arpit Anand <arpit@iof1629d.pnap.ioflood.net>
2022-05-16 18:15:39 +0530

    With custom header

--- client.js
+++ client.js
@@ -7,10 +7,13 @@ const todoPackage = grpcObject.todoPackage;
 
 const client = new todoPackage.TodoService("localhost:40000", grpc.credentials.createInsecure());
 
+var metadata = new grpc.Metadata();
+metadata.add('authorization', 'secret')
+
 client.createTodo({
     "id": 434,
     "text": "this is my text"
-}, (err, response) => {
+}, metadata, (err, response) => {
     console.log("Received from server " + JSON.stringify(response));
 })
 
--- server.js
+++ server.js
@@ -12,12 +12,21 @@ server.bind("0.0.0.0:40000", grpc.ServerCredentials.createInsecure());
 const todos =[];
 server.addService(todoPackage.TodoService.service,{
     "createTodo": (call, callback) => {
+        var myVals = call.metadata.get("authorization"); 
+        var myVal = myVals[0]; 
       const todoItem = {
           "id": todos.length + 1,
           "text": call.request.text
       }
-      todos.push(todoItem);
-      callback(null, todoItem);
+      if(myVal === "secret"){
+        todos.push(todoItem);
+        callback(null, todoItem);
+      }else{
+            callback({
+                "details": "Unauthorized"
+            }, null);
+      }
+      
     },
     "readTodos": (call, callback) => {
         callback(null, {
